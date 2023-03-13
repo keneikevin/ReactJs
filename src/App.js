b1f5c7dcb9a4138ddb0f091dbb0949cd5babb278
keneikevin <kkevinkenei@gmail.com>
2023-03-13 22:45:57 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {User}from './User'
+function App() {
+  const planets = [
+    {name:"Mars",isGasPlanet:false},
+    {name:"Earth",isGasPlanet:false},
+    {name:"Jupiter",isGasPlanet:true},
+    {name:"Venus",isGasPlanet:false},
+    {name:"Neptune",isGasPlanet:true},
+    {name:"Urnus",isGasPlanet:true},
+  ];  
+
+return (
+<div className="App">
+  {planets.map(
+    (planet,key) => planet.isGasPlanet && <h1>{planet.name}</h1>
+  )}
+</div>
+);
+}
+
+
+export default App;
---
+++ src/User.js
@@ -0,0 +1,7 @@
+export const User = (props) => {
+    return (
+      <div>
+        <h1>{props.name}</h1>{  props.isGasPlanet}
+      </div>
+    )
+  }
